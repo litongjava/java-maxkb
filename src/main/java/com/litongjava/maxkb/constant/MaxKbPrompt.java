8b4e83fa00fc43dca341e0e74303bbb1e5561f7b
litongjava <litongjava@qq.com>
2024-11-18 23:42:42 -1000

    update prompt

--- src/main/java/com/litongjava/maxkb/constant/MaxKbPrompt.java
+++ src/main/java/com/litongjava/maxkb/constant/MaxKbPrompt.java
@@ -3,12 +3,15 @@ package com.litongjava.maxkb.constant;
 public interface MaxKbPrompt {
 
   //@formatter:off
-  String image_to_text = "Please extract the pure text from the following image and text\r\n"
-      + "- Ignore header and footer\r\n"
+  String image_to_text = "You are a helpful ocr assiatant. Please extract the text from the following image and text.\r\n"
+      + "- Ignore the header and footer, but exclude the title.\r\n"
       + "- Do not include any additional information or explanations\r\n"
-      + "- Need to recognize mathematical, physical, and chemical symbols and formulas.\r\n"
-      + "- Output only the text portion.\r\n"
+      + "- Need to recognize mathematical, physical, and chemical symbols and formulas and keep format.\r\n"
+      + "- Need to recognize table,title and page number.\r\n"
+      + "- If you encounter an image, display a placeholder for the image.\r\n"
+      + "- Output format is markdown.\r\n"
       + "- If you cannot recognize , please output \"not_working\"\r\n"
-      + "text:";
+      + "\r\n"
+      + "markdown:";
   //@formatter:on
 }
