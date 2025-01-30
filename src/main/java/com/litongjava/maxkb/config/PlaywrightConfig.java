d055b9bfd92532e75907cba581dc31d59ada2bc8
litongjava <litongjava@qq.com>
2025-01-29 18:06:00 -1000

    add kb.playwright.enable

--- src/main/java/com/litongjava/maxkb/config/PlaywrightConfig.java
+++ src/main/java/com/litongjava/maxkb/config/PlaywrightConfig.java
@@ -4,18 +4,22 @@ import com.litongjava.annotation.AConfiguration;
 import com.litongjava.annotation.Initialization;
 import com.litongjava.hook.HookCan;
 import com.litongjava.maxkb.playwright.PlaywrightBrowser;
+import com.litongjava.tio.utils.environment.EnvUtils;
 
 @AConfiguration
 public class PlaywrightConfig {
 
   @Initialization
   public void config() {
-    // 启动
-    PlaywrightBrowser.init();
 
-    // 服务关闭时，自动关闭浏览器和 Playwright 实例
-    HookCan.me().addDestroyMethod(() -> {
-      PlaywrightBrowser.close();
-    });
+    if (EnvUtils.getBoolean("kb.playwright.enable", false)) {
+      // 启动
+      PlaywrightBrowser.init();
+
+      // 服务关闭时，自动关闭浏览器和 Playwright 实例
+      HookCan.me().addDestroyMethod(() -> {
+        PlaywrightBrowser.close();
+      });
+    }
   }
 }
